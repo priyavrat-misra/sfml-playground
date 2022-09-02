d187d877bd81db336b8475e04f76e560abe5abb5
Priyavrat Misra <priyavratmisra123@gmail.com>
2022-09-02 12:58:39 +0530

    add more features

--- Input.cpp
+++ Input.cpp
@@ -2,24 +2,50 @@
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Box", sf::Style::Close);
-	sf::RectangleShape box(sf::Vector2f(50.f, 50.f));
-	box.setFillColor(sf::Color::White);
+	sf::CircleShape box;
+	box.setRadius(25.f);
+	box.setPointCount(3);
+	box.setFillColor(sf::Color(255, 255, 255));
 	box.setOrigin(25.f, 25.f);
 	box.setPosition(320.f, 240.f);
+	float angle = 0.f;
+	std::size_t pointCount = box.getPointCount();
 
 	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-			if (event.type == sf::Event::Closed)
-				window.close();
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::KeyPressed:
+					switch (event.key.code) {
+						case sf::Keyboard::Right:
+							++angle;
+							break;
+						case sf::Keyboard::Left:
+							--angle;
+							break;
+						case sf::Keyboard::Up:
+							pointCount += 1;
+							box.setPointCount(pointCount);
+							break;
+						case sf::Keyboard::Down:
+							pointCount -= pointCount > 3;
+							box.setPointCount(pointCount);
+							break;
+					}
+					break;
+				case sf::Event::Closed:
+					window.close();
+					break;
+			}
+		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 			box.move(-1.f, 0.f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 			box.move(1.f, 0.f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 			box.move(0.f, -1.f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 			box.move(0.f, 1.f);
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 			window.close();
@@ -30,6 +56,7 @@ int main() {
 		}
 
 		window.clear();
+		box.rotate(angle);
 		window.draw(box);
 		window.display();
 	}
